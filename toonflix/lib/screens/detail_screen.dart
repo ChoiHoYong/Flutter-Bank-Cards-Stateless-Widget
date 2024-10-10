5f8f1e65a10ff3074b922c1c5365451bbf3edea1
쿨뽀 <howon5@naver.com>
2024-10-10 19:11:44 +0900

    컴포넌트 위젯 분리
    GestureDetector - 동작감지
    
    detail 페이지로 이동 함수 navigator
    navigator.push를 이용하면 애니메이션을 사용할 수 있어.

---
+++ toonflix/lib/screens/detail_screen.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class DetailScreen extends StatelessWidget {
+  final String title, thumb, id;
+  const DetailScreen({
+    super.key,
+    required this.title,
+    required this.thumb,
+    required this.id,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 1,
+        title: Text(
+          title,
+          style: const TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.green,
+      ),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 50,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 15,
+                      offset: const Offset(10, 10),
+                      color: Colors.black.withOpacity(0.3),
+                    ),
+                  ],
+                ),
+                width: 250,
+                clipBehavior: Clip.hardEdge,
+                child: Image.network(thumb),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
